9ae33664f7b01c40c8a27bc7e5eb1172836d7a37
theMIslam <valvekakaxa@gmail.com>
2023-04-13 18:59:25 +0600

    git flow

--- app/src/main/java/com/example/NoteApp/App.kt
+++ app/src/main/java/com/example/NoteApp/App.kt
@@ -5,3 +5,5 @@ import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
 class App: Application()
+// Создал ветку main и перехожу на dev чтобы стянуть изменения через git pull
+// Стянул сверху написанное через ветку dev и обратно перешёл на main чтобы merge сделатьпш
